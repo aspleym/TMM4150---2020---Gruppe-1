ca7fe9eb51ff38a395f8188808caae5ad6c2f8de
Adrian Skogstad Pleym <adrian@dhcp-10-22-26-199.wlan.ntnu.no>
2020-09-25 09:31:11 +0200

    Read bluetooth signal

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,38 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+int ledPin = 13;
+void blinkLED(int inputVal);
+
 void setup()
 {
-  return;
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  return;
+  // listen for the data from raspberry pi
+  if (Serial.available() > 0)
+  {
+    // read a numbers from serial port
+    int inputVal = Serial.parseInt();
+    if (inputVal > 0)
+    {
+      //Serial.print("Your input is: ");
+      //Serial.println(String(inputVal));
+      // Here blink the LED
+      blinkLED(inputVal);
+    }
+  }
+}
+
+void blinkLED(int inputVal)
+{
+  for (int i = 0; i < inputVal; i++)
+  {
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    delay(500);
+  }
 }
